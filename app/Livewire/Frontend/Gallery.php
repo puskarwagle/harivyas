4d9bc368bd01b1a92117b36e0df69c76d12e8447
puskarwagl <puskarwagle17@gmail.com>
2025-07-16 04:15:38 +0530

    gallery bugs fixed

--- app/Livewire/Frontend/Gallery.php
+++ app/Livewire/Frontend/Gallery.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire\Frontend;
 
+use App\Models\GalleryImage;
 use Livewire\Component;
 
 class Gallery extends Component
 {
+    public array $images = [];
+
+    public function mount()
+    {
+        $this->images = $this->loadGalleryImages();
+    }
+
+    protected function loadGalleryImages(): array
+    {
+        return GalleryImage::with('post')->get()->map(function ($image) {
+            return [
+                'id' => $image->id,
+                'url' => $image->url,
+                'title' => $image->post->title,
+                'description' => $image->post->description,
+                'tags' => $image->post->tags ?? [],
+            ];
+        })->toArray();
+    }
+
     public function render()
     {
-        return view('livewire.frontend.gallery');
+        return view('livewire.frontend.gallery', [
+            'images' => $this->images,
+        ]);
     }
 }
